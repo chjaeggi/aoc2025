a30dbb7e350e43a6d174575273a41b599a6adf51
jaeggi <chr.jaeggi@gmail.com>
2025-12-01 08:58:55 +0100

    day1 solutions

--- src/days/Day1.kt
+++ src/days/Day1.kt
@@ -1,9 +1,39 @@
 package days
 
 import utils.execFileByLine
-import kotlin.math.abs
 
 class Day1 {
+
+    var currentPosition = 50
+    val positions = mutableListOf(currentPosition)
+    var hitZero = 0
     fun solve() {
+        execFileByLine(1) {
+            val isLeft = it.contains("L")
+            var rotateBy = it.removePrefix(if (isLeft) "L" else "R").toInt()
+
+            hitZero += rotateBy / 100
+            rotateBy %= 100
+
+            val newPositionBeforeModulo = if (isLeft) currentPosition - rotateBy else currentPosition + rotateBy
+
+            if (newPositionBeforeModulo == 0 || newPositionBeforeModulo == 100) {
+                hitZero++
+            } else if (newPositionBeforeModulo < 0 && positions.last() != 0) {
+                hitZero++
+            } else if (newPositionBeforeModulo > 100 && positions.last() != 0) {
+                hitZero++
+            }
+
+            currentPosition = newPositionBeforeModulo
+            if (currentPosition < 0) {
+                currentPosition += 100
+            }
+            currentPosition %= 100
+
+            positions.add(currentPosition)
+        }
+        println(positions.count { it == 0 })
+        println(hitZero)
     }
 }
