c04e6439f0e14ad75198971a5f9aa9f659c96e3a
jaeggi <chr.jaeggi@gmail.com>
2025-12-06 19:39:34 +0100

    revert fileparsing

--- src/utils/FileParsing.kt
+++ src/utils/FileParsing.kt
@@ -1,23 +1,43 @@
 package utils
 
+import java.io.BufferedReader
 import java.io.File
+import java.io.FileReader
+import kotlin.math.min
+
 
 fun execFileByLine(number: Int, f: (str: String) -> Unit) =
     File("./src/inputs/input$number.txt").forEachLine { f(it) }
 
 fun execFileByLineIndexed(number: Int, f: (index: Int, str: String) -> Unit) {
-    File("./src/inputs/input$number.txt").readLines().forEachIndexed(f)
+    val arr = mutableListOf<String>()
+    File("./src/inputs/input$number.txt").forEachLine {
+        arr.add(it)
+    }
+    arr.forEachIndexed { index, value ->
+        f(index, value)
+    }
 }
 
 fun execFileByLineInGroups(number: Int, groupSize: Int, f: (str: List<String>) -> Unit) {
-    val lines = File("./src/inputs/input$number.txt").readLines()
-    lines.chunked(groupSize).forEach(f)
+    val arr = ArrayDeque<String>()
+    File("./src/inputs/input$number.txt").forEachLine {
+        arr.addLast(it)
+    }
+    while (arr.isNotEmpty()) f((0..<min(groupSize, arr.size)).map { arr.removeFirst() })
 }
 
 fun numberOfLinesPerFile(number: Int): Int {
-    return File("./src/inputs/input$number.txt").readLines().size
+    val reader = BufferedReader(FileReader("./src/inputs/input$number.txt"))
+    var lines = 0
+    while (reader.readLine() != null) lines++
+    reader.close()
+    return lines
 }
 
 fun numberOfCharsPerLine(number: Int): Int {
-    return File("./src/inputs/input$number.txt").useLines { it.firstOrNull()?.length ?: 0 }
+    val reader = BufferedReader(FileReader("./src/inputs/input$number.txt"))
+    val line = reader.readLine()
+    reader.close()
+    return line.length
 }
