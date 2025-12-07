be736af6c521f36fb1cfffca55ee0074d30ed64b
jaeggi <chr.jaeggi@gmail.com>
2025-12-07 14:27:59 +0100

    shorten it

--- src/days/Day7.kt
+++ src/days/Day7.kt
@@ -19,28 +19,18 @@ class Day7 {
         println(runBeam(start!!))
         println(nOfPaths(start!!))
     }
-
-    private fun runBeam(
-        pos: Point2D,
-        splitPoints: MutableSet<Point2D> = mutableSetOf(),
-        visited: MutableSet<Point2D> = mutableSetOf(),
-    ): Int {
-        if (manifold.atOrNull(pos.s) == '.') {
-            runBeam(pos.s, splitPoints, visited)
+    
+    
+    private fun runBeam(pos: Point2D, visited: MutableSet<Point2D> = mutableSetOf()): Int {
+        if (!manifold.inBounds(pos.s) || !visited.add(pos.s)) {
+            return 0
         }
-        if (manifold.atOrNull(pos.s) == '^') {
-            splitPoints += pos.s
 
-            for (d in listOf(pos.sw, pos.se)) {
-                manifold.atOrNull(d)?.let {
-                    if (d !in visited) {
-                        visited += d
-                        runBeam(d, splitPoints, visited)
-                    }
-                }
-            }
+        return when (manifold.at(pos.s)) {
+            '.' -> runBeam(pos.s, visited)
+            '^' -> 1 + runBeam(pos.sw, visited) + runBeam(pos.se, visited)
+            else -> 0
         }
-        return splitPoints.size
     }
 
     private fun nOfPaths(pos: Point2D, cache: MutableMap<Point2D, Long> = mutableMapOf()): Long {
